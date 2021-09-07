a7f94c10a48afd51b3396a6502c243af39a9919a
AFishyOcean <dljhazelh@gmail.com>
2021-09-07 09:55:08 -0400

    Made several shapes including a star

--- shapes.py
+++ shapes.py
@@ -1,9 +1,50 @@
 import turtle
-turtle.forward(200)
+turtle.hideturtle()
+turtle.speed(5)
+# goto for spacing shapes
+turtle.penup()
+turtle.goto(50,100)
+turtle.pendown()
+# square
+turtle.forward(100)
 turtle.right(90)
-turtle.forward(200)
+turtle.forward(100)
 turtle.right(90)
-turtle.forward(200)
+turtle.forward(100)
 turtle.right(90)
-turtle.forward(200)
+turtle.forward(100)
+# goto
+turtle.penup()
+turtle.goto(150,-100)
+turtle.pendown()
+# circle
+turtle.circle(50)
+# goto
+turtle.penup()
+turtle.goto(-50,-150)
+turtle.pendown()
+# triangle
+turtle.left(90)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+# goto
+turtle.penup()
+turtle.goto(-75,0)
+turtle.pendown()
+# pentagon
+turtle.forward(75)
+turtle.right(72)
+turtle.forward(75)
+turtle.right(72)
+turtle.forward(75)
+turtle.right(72)
+turtle.forward(75)
+turtle.right(72)
+turtle.forward(75)
+turtle.right(72)
+# view product
 turtle.exitonclick()
---
+++ star.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.hideturtle()
+turtle.speed(5)
+# star
+turtle.setheading(290)
+turtle.fd(200)
+turtle.rt(144)
+turtle.fd(200)
+turtle.rt(144)
+turtle.fd(200)
+turtle.rt(144)
+turtle.fd(200)
+turtle.rt(144)
+turtle.fd(200)
+turtle.rt(144)
+
+turtle.exitonclick()
