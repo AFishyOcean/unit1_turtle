cf29bb613ebcbc1da484fbd266f36d42ec8500bc
AFishyOcean <dljhazelh@gmail.com>
2021-09-13 14:02:38 -0400

    Made several shapes including assignment one octagon

--- assignment_one.py
+++ assignment_one.py
@@ -1,7 +1,17 @@
 import turtle
+turtle.speed(10)
 turtle.hideturtle()
+turtle.color("blue")
+turtle.rt(90)
+for x in range(12):
+    for x in range(6):
+        turtle.forward(100)
+        turtle.rt(60)
+    turtle.rt(30)
+turtle.color("black")
+turtle.penup()
+turtle.goto(-120, 0)
+turtle.pendown()
+turtle.circle(120)
 
-for x in range(8):
-    turtle.fd(100)
-    turtle.rt(45)
 turtle.exitonclick()
